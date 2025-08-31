c9e1f6e0381328bd000e01619ee1203642e1fecf
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-08-31 20:41:35 -0300

    Update MainApp.java

--- POO/prj-formas-geometricas/src/main/java/org/example/Main.java
+++
@@ -1,4 +0,0 @@
-package org.example;
-    public static void main(String[] args) {
-    }
-}
---
+++ POO/prj-formas-geometricas/src/main/java/org/example/MainApp.java
@@ -0,0 +1,11 @@
+package org.example;
+    public static void main(String[] args) {
+        // Intanciação do Circulo
+        Circulo circulo = new Circulo();
+        // Intanciação do Triangulo
+        Triangulo triangulo = new Triangulo();
+        // Intanciação do Quadrado
+        Quadrado quadrado = new Quadrado();
+
+    }
+}
