00164d872c6eb624ef4d5e52de62b8ed11fe4ff9
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-08-31 20:34:58 -0300

    Criado as Classes
    Criado as classes Circulo, Triangulo e Quadrado

---
+++ POO/prj-formas-geometricas/src/main/java/org/example/Quadrado.java
@@ -0,0 +1,4 @@
+package org.example;
+public class Quadrado {
+    public double lado;
+}
---
+++ POO/prj-formas-geometricas/src/main/java/org/example/Triangulo.java
@@ -0,0 +1,5 @@
+package org.example;
+public class Triangulo {
+    public double base;
+    public double altura;
+}
