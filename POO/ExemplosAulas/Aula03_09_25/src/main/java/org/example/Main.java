2e948640662009122858e1afd95c5f4df9600be0
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-03 14:50:05 -0300

    Update Main.java

--- POO/ExemplosAulas/Aula03_09_25/src/main/java/org/example/Main.java
+++ POO/ExemplosAulas/Aula03_09_25/src/main/java/org/example/Main.java
@@ -1,6 +1,9 @@
 package org.example;
 public class Main {
     public static void main(String[] args) {
-        // IDE - Integrated desenvolviment enviroment
+        // IDE - Integrated Desenvolviment Enviroment
+        // Nomes de Classes sempre com a primeira letra maiuscula
+        // E sempre no singular
+        // Nome de arquivo e de classe devem ser iguais
     }
 }
