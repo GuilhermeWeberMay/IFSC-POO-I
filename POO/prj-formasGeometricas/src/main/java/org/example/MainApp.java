0578eb732e00a250ab870083b3d0553f681eaf76
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-02 21:14:14 -0300

    Update MainApp.java

--- POO/prj-formasGeometricas/src/main/java/org/example/MainApp.java
+++ POO/prj-formasGeometricas/src/main/java/org/example/MainApp.java
@@ -2,14 +2,18 @@ package org.example;
 public class MainApp {
     public static void main(String[] args) {
         // Instanciação do Objeto Circulo
-        Circulo circulo = new Circulo();
+        Circulo circulo = new Circulo(5);
         // Instanciação do Objeto Triangulo
-        Triangulo triangulo = new Triangulo();
+        Triangulo triangulo = new Triangulo(3,4);
         // Instanciação do Objeto Quadrado
-        Quadrado quadrado = new Quadrado();
+        Quadrado quadrado = new Quadrado(2);
 
         circulo.desenhar();
         triangulo.desenhar();
         quadrado.desenhar();
+
+        System.out.println("Área do círculo: "+ circulo.calcularArea());
+        System.out.println("Área do triângulo: "+ triangulo.calcularArea());
+        System.out.println("Área do quadrado: "+quadrado.calcularArea());
     }
 }
