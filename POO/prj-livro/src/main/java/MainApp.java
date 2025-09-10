c7dc54ae86abf93cbc84493a280cd4ca1ff60f42
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-10 09:37:41 -0300

    atividade do livro inciada

---
+++ POO/prj-livro/src/main/java/MainApp.java
@@ -0,0 +1,13 @@
+public class MainApp {
+    public static void main(String[] args) {
+        // Criação e instanciação do objeto
+        Livro livro = new Livro();
+        // Criação, instanciação e inicialização do objeto
+        Livro livro2 = new Livro(1, "Pequeno Principe", "1234567891011", "Clarice Lispector", "Maquinaria",
+                                5.99f, 25.00f);
+        livro2.setPrecoCusto(10);
+        System.out.println(livro2.getPrecoCusto());
+        System.out.println(livro2.getMargemLucro()); // está apresentando a margem de lucro 0.02
+        System.out.print(livro.getPrecoVenda());
+    }
+}
