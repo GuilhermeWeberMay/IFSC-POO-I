7b2a05e387f6674bf9d6e8b8ef96647d6aa4298f
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-10 14:58:41 -0300

    Anotações Aula10_09_25

---
+++ POO/ExemplosAulas/Aula10_09_25/src/main/java/MainApp.java
@@ -0,0 +1,28 @@
+public class MainApp {
+    public static void main(String[] args) {
+//      Exemplo
+//      Instanciação - construtor de classe padrão - Caneta() é o contrustor padrão
+//          Caneta c1 = new Caneta();
+//          c1.cor = "Azul";
+//          c1.ponta = 0.05f;
+//      Método de criação do método construtor padrão
+//          public Caneta(){
+//            this.cor = null;
+//            this.ponta = 0.0f
+//          }
+//      Como colocar uma regra de negócio - assim você obriga o usuario a inserir uma informação
+//          public Caneta(String cor, float espessura){
+//            this.cor = cor;
+//            this.espessura = cor
+//        }
+//       Pode ter mais de um método construtor com todos os atributos ou somente alguns
+//          public Caneta(String cor){
+//            this.cor = cor;
+//       }
+//       Não é possivel fazer uma sobrecarga de um objeto com o mesmo tipo de variavel primitiva
+//       Construtor Padrão do Java - sem inicializar nenhuma regra de negócio
+//           public Caneta(){
+//             //Vazio
+//        }
+    }
+}
