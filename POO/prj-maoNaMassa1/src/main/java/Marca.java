bee82cee8996c13a5702f527e977604f2a1a3d3d
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-09-30 09:30:37 -0300

    criado outras classes

---
+++ POO/prj-maoNaMassa1/src/main/java/Marca.java
@@ -0,0 +1,12 @@
+public class Marca {
+    private int id;
+    private String nome;
+
+    public Marca(){
+        this.id = id;
+        this.nome = nome;
+    }
+    public Marca(String nome){
+        this.nome = nome;
+    }
+}
