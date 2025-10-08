db6122b3d258a53093167662baac6c2f9846ffc0
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-10-08 17:28:54 -0300

    anotacoes aula 08 10 2025

---
+++ POO/Aula08_10_25/src/main/java/Anotacoes.java
@@ -0,0 +1,10 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+public class Anotacoes {
+//    date javautil - atributo
+//    date sql - usa em banco de dados
+    private LocalDateTime dataHorario;
+    private LocalDate dataInicio;
+}
