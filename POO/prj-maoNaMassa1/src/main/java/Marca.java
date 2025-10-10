11ddabfe4cdbbf7c0b53e2d488090a714eb985ea
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-10-10 09:40:07 -0300

    Add constructors and accessors to Marca class
    Introduces full and empty constructors, as well as getter and setter methods for the 'id' and 'nome' fields in the Marca class to improve object initialization and encapsulation.

--- POO/prj-maoNaMassa1/src/main/java/Marca.java
+++ POO/prj-maoNaMassa1/src/main/java/Marca.java
@@ -2,6 +2,33 @@ public class Marca {
     private int id;
     private String nome;
 
+    // Métodos contrutores
+
+    // Método construtor completo
+    public Marca(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    // Método construtor vazio
+    public Marca() {
+    }
+
+    // Métodos GETTERS e SETTERS
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Método de apresentação do objeto - Reescreve o método toString();
     @Override
     public String toString() {
         return "Marca{" +
