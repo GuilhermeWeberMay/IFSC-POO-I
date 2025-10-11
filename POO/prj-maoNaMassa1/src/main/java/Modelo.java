480b9406c0199c060c17635b643943dace30680e
Guilherme Weber May <guilhermewebermay.dev@gmail.com>
2025-10-11 19:18:42 -0300

    Add getters and setters to Modelo class
    Introduced getter and setter methods for categoria, marca, descricao, id, and motor fields in the Modelo class to improve encapsulation and provide controlled access to these properties.

--- POO/prj-maoNaMassa1/src/main/java/Modelo.java
+++ POO/prj-maoNaMassa1/src/main/java/Modelo.java
@@ -16,6 +16,38 @@ public class Modelo {
         this.motor = new Motor(potencia,tipoCombustivel);
     }
 
+    public ECategoria getCategoria() {
+        return categoria;
+    }
+    public void setCategoria(ECategoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
     @Override
     public String toString() {
         return "Modelo{" +
